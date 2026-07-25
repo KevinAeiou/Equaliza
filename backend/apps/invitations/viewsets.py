9a45a4e61402cca65c5c64c1157a3674c10556a4
Kevin <gbdev05@gbringel.com>
2026-07-25 13:14:17 -0400

    fix: Corrigir parâmetros ao criar um convite.

--- backend/apps/invitations/viewsets.py
+++ backend/apps/invitations/viewsets.py
@@ -41,7 +41,7 @@ class InvitationViewSet(viewsets.ModelViewSet):
 
         invitation = CreateInvitationService.execute(
             user=request.user,
-            data=serializer.validated_data["email"],
+            email=serializer.validated_data["email"],
         )
 
         return Response(
