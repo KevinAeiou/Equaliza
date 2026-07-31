39ad7903cdc457ef0dfb1fb7e30c06395a1f4a41
Kevin <gbdev05@gbringel.com>
2026-07-31 13:40:59 -0400

    chore: Ajustar responsividade de tabelas.

--- frontend/src/components/dataTable/index.tsx
+++ frontend/src/components/dataTable/index.tsx
@@ -11,6 +11,7 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/src/components/ui/table"
+import { ScrollArea } from "../ui/scroll-area"
 
 interface DataTableProps<TData> {
 	table: TanStackTable<TData>
@@ -26,58 +27,60 @@ export function DataTable<TData>({
 	emptyMessage = "Nenhum registro encontrado.",
 }: DataTableProps<TData>) {
 	return (
-		<Table>
-			<TableHeader>
-				{table.getHeaderGroups().map((headerGroup) => (
-					<TableRow key={headerGroup.id}>
-						{headerGroup.headers.map((header) => (
-							<TableHead key={header.id}>
-								{header.isPlaceholder
-									? null
-									: flexRender(
-										header.column.columnDef.header,
-										header.getContext()
-									)}
-							</TableHead>
-						))}
-					</TableRow>
-				))}
-			</TableHeader>
-
-			<TableBody>
-				{loading ? (
-					<TableRow>
-						<TableCell
-							colSpan={table.getVisibleLeafColumns().length}
-							className="h-24 text-center"
-						>
-							{loadingMessage}
-						</TableCell>
-					</TableRow>
-				) : table.getRowModel().rows.length ? (
-					table.getRowModel().rows.map((row) => (
-						<TableRow key={row.id}>
-							{row.getVisibleCells().map((cell) => (
-								<TableCell key={cell.id}>
-									{flexRender(
-										cell.column.columnDef.cell,
-										cell.getContext()
-									)}
-								</TableCell>
+		<ScrollArea className="flex-1 w-full h-full">
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => (
+								<TableHead key={header.id}>
+									{header.isPlaceholder
+										? null
+										: flexRender(
+											header.column.columnDef.header,
+											header.getContext()
+										)}
+								</TableHead>
 							))}
 						</TableRow>
-					))
-				) : (
-					<TableRow>
-						<TableCell
-							colSpan={table.getVisibleLeafColumns().length}
-							className="h-24 text-center"
-						>
-							{emptyMessage}
-						</TableCell>
-					</TableRow>
-				)}
-			</TableBody>
-		</Table>
+					))}
+				</TableHeader>
+
+				<TableBody>
+					{loading ? (
+						<TableRow>
+							<TableCell
+								colSpan={table.getVisibleLeafColumns().length}
+								className="h-24 text-center"
+							>
+								{loadingMessage}
+							</TableCell>
+						</TableRow>
+					) : table.getRowModel().rows.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow key={row.id}>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>
+										{flexRender(
+											cell.column.columnDef.cell,
+											cell.getContext()
+										)}
+									</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell
+								colSpan={table.getVisibleLeafColumns().length}
+								className="h-24 text-center"
+							>
+								{emptyMessage}
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+		</ScrollArea>
 	)
 }
